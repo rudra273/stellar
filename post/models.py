a54502849dadb63566408504d4b4d98645f64f05
rudra273 <rudramohanty45@gmail.com>
2024-02-24 15:06:53 +0530

    newww

--- post/models.py
+++ post/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class Post(models.Model): 
+class Post(models.Model):
     # user = models.ForeignKey(User, on_delete=models.CASCADE)
     caption = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -10,10 +10,14 @@ class Post(models.Model):
     def __str__(self):
         return self.caption
 
-class PostImage(models.Model):
-    post = models.ForeignKey(Post, related_name='images', on_delete=models.CASCADE)
-    image = models.ImageField(upload_to='posts/')
-    created_at = models.DateTimeField(auto_now_add=True)  
+# class Media(models.Model):
+#     file = models.FileField(upload_to='media/')
+#     post = models.ForeignKey(Post, related_name='media', on_delete=models.CASCADE)
+#     created_at = models.DateTimeField(auto_now_add=True)
+
+#     def __str__(self):
+#         return self.file.name
+
 
 
 # class Like(models.Model):
---
+++ post/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from .models import  *
+# Create your views here.
+def post_index(request):
+    posts = Post.objects.all()
+
+    context = { 
+        'posts': posts
+    }
+
+    return render(request, 'post/post_index.html', context)
